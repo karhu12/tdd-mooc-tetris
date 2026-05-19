370822e4e24435d43421db1dbf4253f7e1be0fa3
Riku Kaipainen <riku.kaipainen@gmail.com>
2026-05-19 13:28:33 +0300

    I_SHAPE implementation

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -4,4 +4,10 @@ export class Tetromino {
     static T_SHAPE = new RotatingShape(`.T.
                                         TTT
                                         ...`);
+
+    static I_SHAPE = new RotatingShape(`.....
+                                        .....
+                                        IIII.
+                                        .....
+                                        .....`);
 }
